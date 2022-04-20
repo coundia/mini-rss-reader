a9f86a1e300766310d4fc2c7a6fe0b4d07af556d
coundia <papacoundia@gmail.com>
2022-04-20 23:11:14 +0000

    add express

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ app.use(express.static(__dirname + '/dist/mini-rss-reader'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/mini-rss-reader/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+
+const server = app.listen(process.env.PORT || 8080, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+});
