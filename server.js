68f47c0d534f75831eb2965739ea8ea8fc53f759
coundia <papacoundia@gmail.com>
2022-04-24 12:27:10 +0000

    fix heroku deploy

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 //autoriser le proxy  de heroku
+/*
 app.set('trust proxy', 1);
 // limiter le nb de requete
 const RateLimit = require('express-rate-limit');
-const limiter = new RateLimit({
+const limiter =   RateLimit({
   windowMs: 60*1000, // 1 minute
   max: 10
 });
 //Pour que pour heroku marche
 app.use(limiter);
-
+ */
 app.use(express.static(__dirname + '/dist/mini-rss-reader'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/mini-rss-reader/index.html'));
