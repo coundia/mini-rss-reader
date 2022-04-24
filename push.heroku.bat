1bae53501c7e23d7ab8f09d6c52955a2e8436fbd
coundia <papacoundia@gmail.com>
2022-04-24 13:13:50 +0000

    deploy to heroku a

--- push.heroku.bat
+++ push.heroku.bat
@@ -1,1 +1,9 @@
-git push heroku main:main
+@echo off
+
+ git add .
+"deploy to heroku ${date} "
+SET message=Deploy to heroku at %date%
+ git commit -m "deploy to heroku a  "
+ git push heroku main:main
+
+echo %message%
--- server.js
+++ server.js
@@ -2,17 +2,24 @@ const express = require('express');
 const path = require('path');
 const app = express();
 //autoriser le proxy  de heroku
-/*
+
 app.set('trust proxy', 1);
 // limiter le nb de requete
-const RateLimit = require('express-rate-limit');
-const limiter =   RateLimit({
-  windowMs: 60*1000, // 1 minute
-  max: 10
-});
+const rateLimit = require('express-rate-limit')
+
+//  creer un role
+const apiRequestLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 1000,//mille req par minute
+  handler: function (req, res, /*next*/) {
+    return res.status(429).json({
+      error: 'Trop de requetes ,Attend 1 minute et ressayer'
+    })
+  }
+})
 //Pour que pour heroku marche
-app.use(limiter);
- */
+app.use(apiRequestLimiter)
+
 app.use(express.static(__dirname + '/dist/mini-rss-reader'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/mini-rss-reader/index.html'));
